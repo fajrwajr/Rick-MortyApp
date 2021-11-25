bae6eb93ec1751d5240921d3ddfe58110b042d52
fajrwajr <bookreaderfajr@gmail.com>
2021-11-25 06:20:07 +0000

    Added conditional

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import rickandmorty from "../../img/logo.png";
+import "../../styles/home.scss";
 
 export const Navba = () => {
 	return (
-		<Navbar bg="warning" variant="dark">
-			<Container>
-				<Navbar.Brand href="#home">Welcome to the Citadel</Navbar.Brand>
-			</Container>
-		</Navbar>
+		<header className="header">
+			<div className="flex">
+				<div className="logo">
+					<img src={rickandmorty} />
+				</div>
+				<Navbar className="menu">
+					<Container>
+						<Nav>
+							<Nav.Link href="#">About</Nav.Link>
+							<Nav.Link href="#">Characters</Nav.Link>
+							<Nav.Link href="#">Rick</Nav.Link>
+							<Nav.Link href="#">Morty</Nav.Link>
+						</Nav>
+					</Container>
+				</Navbar>
+
+				<div className="text">
+					<h1>Rick and Morty</h1>
+					<p>NO YOURE RIGHT. LETS DO IT THE DUMBEST WAY POSSIBLE BECUASE THATS EASIER FOR YOU - Rick</p>
+				</div>
+			</div>
+		</header>
 	);
 };
